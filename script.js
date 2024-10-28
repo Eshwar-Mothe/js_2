1a7eece6ae78917d28968e8f00b4800547494ed1
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2024-10-29 00:07:43 +0530

    frst commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var a = 10;
+var b = 12;
+
+console.log("Addtion of " + a + "," + b + "is: " +(a+b))
+console.log("Subtration of " + a + "," + b + "is: " +(a-b))
+console.log("Multiplication of " + a + "," + b + "is: " +(a*b))
+console.log("Ratio of " + a + "," + b + "is: " +(a/b))
+
+
+// let a = 10; //if this was decareled like this we will get error as "SyntaxError: Identifier 'a' has already been declared"
+// let b = 20; //if this was decareled like this we will get error as "SyntaxError: Identifier 'b' has already been declared"
+// console.log("Addtion of " + a + "," + b + "is: " +(a+b))
+// console.log("Subtration of " + a + "," + b + "is: " +(a-b))
+
+// const a = 10;//if this was decareled like this we will get error as "SyntaxError: Identifier 'a' has already been declared"
+// const b = 12;//if this was decareled like this we will get error as "SyntaxError: Identifier 'b' has already been declared"
+
+// console.log("Addtion of " + a + "," + b + "is: " +(a+b))
+// console.log("Subtration of " + a + "," + b + "is: " +(a-b))
